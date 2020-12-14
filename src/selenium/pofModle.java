32f30da8e93cd5dac63f8759d80b15f300cce45c
roi32 <75878108+roi32@users.noreply.github.com>
2020-12-14 20:47:19 +0200

    1

--- src/selenium/pofModle.java
+++ src/selenium/pofModle.java
@@ -22,8 +22,7 @@ public class pofModle {
 	WebElement add;
 	@FindBy(how = How.ID,using = "quantity")
 	WebElement quantity;
-	@FindBy(how = How.CLASS_NAME,using = "first")
-	WebElement first;
+
 	@FindBy(how = How.XPATH,using = "//table/tbody/tr[1]/td//tbody/tr[3]/td//form/div[6]//div[2]/div[4]//tbody/tr[2]/td[10]/a")
 	WebElement remove;
 	@FindBy(how = How.XPATH,using = "//table/tbody/tr[1]/td//tbody/tr[3]/td//form/div[6]//div[2]/div[4]//tbody/tr/td[7]")
@@ -36,7 +35,7 @@ public class pofModle {
 	WebElement Carbo2;
 	@FindBy(how = How.XPATH,using = "//table/tbody/tr[1]/td/table/tbody/tr[3]/td//form/div[6]//div[2]/div[5]/table//tr[3]/td[2]")
 	WebElement sumCarbo;
-	@FindBy(how = How.XPATH,using = "//div[2]/div[1]/div/div[3]/main/div/div/div/div/div/div/nav/ul/li")
+	@FindBy(how = How.CLASS_NAME,using = "pager__item")
 	List<WebElement> pages;
 	@FindBy(how = How.XPATH,using = "//div[2]/div[1]/div/div[3]/main/div/div/div/div/div/div/div[2]/div/table/tbody/tr/td[1]")
 	List<WebElement> jobs;
--- src/selenium/t7_1.java
+++ src/selenium/t7_1.java
@@ -17,7 +17,6 @@ public class t7_1 {
 	
 	 static WebDriver driver;
 	 static pofModle pof;
-
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\temp\\chromedriver.exe");
@@ -25,225 +24,171 @@ public class t7_1 {
 	    driver=new ChromeDriver();
 	    driver.manage().window().maximize();
 		driver.get("http://www.netogreen.co.il/Calculators/Nutrition-Calories.aspx");
-	    
 	}
+	
 	@Before
 	public  void befor() throws Exception {
 		pof = new pofModle();
-		driver.navigate().refresh();
-		Thread.sleep(2000);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		pof = PageFactory.initElements (driver, pofModle.class);
-		
-
-	}	
+	}
+	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		driver.quit();
-	
 	}
-	@Test
-	public void test001() {
-		try {
 
+	@Test
+	public void test001() throws InterruptedException {
 			pof.searchTxt.sendKeys("לחם");
 			pof.Button1.click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			String resultString= pof.result.get(0).getText();
 			if (resultString.contains("לחם")) {
 				System.out.println("test1 pass");
-			}
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}	
+			}else {
+				throw new RuntimeException();
+			}	
 	}	
-
+	
 	@Test
 	public void test002() throws InterruptedException {
-		try {
+			pof.searchTxt.clear();
 			pof.searchTxt.sendKeys("123");
 			pof.Button1.click();
 			Thread.sleep(2000);
 			String resultString= pof.result.get(0).getText();
 			if (resultString.contains("לא נמצאו מזונות, נסה שנית.")) {
 				System.out.println("test2 pass");
+			}else {
+				throw new RuntimeException();
 			}
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}
 	}
 	
-
 	@Test
-	public void test003() {
-		try {
+	public void test003() throws InterruptedException {
+			pof.searchTxt.clear();
 			pof.searchTxt.sendKeys("לחם");
 			pof.Button1.click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 		    pof.result.get(3).click();
 		    pof.add.click();
 		    Alert alert = driver.switchTo().alert();
-		   String alartString= alert.getText();	
-			Thread.sleep(2000);
+		    String alartString= alert.getText();	
+			Thread.sleep(1000);
 			if (alartString.contains("יש לבחור  יחידת מידה וכמות")) {
 				System.out.println(alartString);
 				System.out.println("test3 pass");
-
+			}else {
+				throw new RuntimeException();
 			}
-			driver.switchTo().defaultContent();
-		} catch (Exception e) {
-		}
-		
+			driver.switchTo().alert().accept();
 	}
 
 	@Test
-	public void test004() {
-		try {
+	public void test004() throws InterruptedException {
 			pof.quantity.sendKeys("wewdsfasq");
-			Thread.sleep(5000);
+			Thread.sleep(1000);
 			String quantityString=pof.quantity.getText();
-			
 			if(quantityString.isEmpty()){
 				   System.out.println("test4 pass"); 
-			}			
-		} catch (Exception e) {
-		}
+			}	else {
+				throw new RuntimeException();
+			}		
+		
 		
 	}
-
+	
 	@Test
-	public void test005() {
-		try {
-			pof.searchTxt.sendKeys("לחם");
-			pof.Button1.click();
-			Thread.sleep(2000);
-			pof.result.get(3).click();
-			Thread.sleep(2000);
+	public void test005() throws InterruptedException {
+		
 			pof.quantity.sendKeys("900000000");
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			pof.add.click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			Alert alert = driver.switchTo().alert();
 			String alartString= alert.getText();
 			if (alartString.contains("קוד: 3001")) {
 				System.out.println(alartString);
 				System.out.println("test5 pass");
+			}else {
+				throw new RuntimeException();
 			}
-			
-			driver.switchTo().defaultContent();
-
-		} catch (Exception e) {
-		}
+			driver.switchTo().alert().accept();
+	}
+	
+	@Test
+	public void test006() throws InterruptedException {
 		
-	}
-
-	@Test
-	public void test006() {
-		try {
-			pof.searchTxt.sendKeys("לחם");
-			pof.Button1.click();
-			Thread.sleep(2000);
-			pof.result.get(3).click();
-			Thread.sleep(2000);
+			pof.quantity.clear();
+			Thread.sleep(1000);
 			pof.quantity.sendKeys("5");
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			pof.add.click();
-			Thread.sleep(2000);
-			if (pof.first.isDisplayed()) {
+			Thread.sleep(1000);
+			if (pof.resultes.size()==2) {
 				System.out.println("test6 pass");
+			}else {
+				throw new RuntimeException();
 			}
-
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}
 	}
 
 	@Test
-	public void test007() {
-		try {
+	public void test007() throws InterruptedException {
+		
+			pof.searchTxt.clear();
+			pof.quantity.clear();
 			pof.searchTxt.sendKeys("לחם");
 			pof.Button1.click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			pof.result.get(0).click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			pof.quantity.sendKeys("2");
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			pof.add.click();
-			Thread.sleep(5000);
-			if (pof.first.isDisplayed()) {
+			Thread.sleep(1000);
+			if (pof.resultes.size()==3) {
 				System.out.println("test7 pass");
+			}else {
+				throw new RuntimeException();
 			}	
-
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}
 	}
 
-
 	@Test
-	public void test008() {
-		try {
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			pof.searchTxt.sendKeys("לחם");
-			pof.Button1.click();
-			Thread.sleep(2000);
-			pof.result.get(3).click();
-			Thread.sleep(2000);
-			pof.quantity.sendKeys("2");
-			Thread.sleep(2000);
-			pof.add.click();
-			Thread.sleep(2000);
-			pof.result.get(4).click();
-			pof.add.click();
-			Thread.sleep(2000);
+	public void test008() throws InterruptedException {
+		
 			pof.remove.click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			if (pof.resultes.size()<3) {
 				System.out.println("test8 pass");
+			}else {
+				throw new RuntimeException();
 			}
 
-		} catch (Exception e) {
-		}
 	}
-
 	@Test
-	public void test009() {
-		try {
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	public void test009() throws InterruptedException {
+		
+			pof.searchTxt.clear();
+			pof.quantity.clear();
 			pof.searchTxt.sendKeys("לחם");
 			pof.Button1.click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			pof.result.get(5).click();
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			pof.quantity.sendKeys("2");
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 			pof.add.click();
-			if (pof.first.isDisplayed()) {
+			Thread.sleep(1000);
+			if (pof.resultes.size()==3) {
 				System.out.println("test9 pass");
-
+			}else {
+				throw new RuntimeException();
 			}
-
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}
 	}
 
 	@Test
 	public void test010() {
-		try {
-			pof.searchTxt.sendKeys("לחם");
-			pof.Button1.click();
-			Thread.sleep(2000);
-			pof.result.get(3).click();
-			Thread.sleep(2000);
-			pof.quantity.sendKeys("2");
-			Thread.sleep(2000);
-			pof.add.click();
-			Thread.sleep(2000);
-			pof.result.get(3).click();
-			pof.result.get(4).click();
-			Thread.sleep(2000);
-			pof.add.click();
-			Thread.sleep(2000);
 			String first=pof.caloris.get(0).getText();
 			int first1=Integer.parseInt(first);
 			String secand=pof.caloris.get(1).getText();
@@ -254,30 +199,13 @@ public class t7_1 {
 			if (sumCaloris1==sum) {
 				System.out.println("הקלריות שוות");
 				System.out.println("test10 pass");
-			}	
-
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}
+			}else {
+				throw new RuntimeException();
+			}
 	}
 
 	@Test
 	public void test011() {
-		try {
-			pof.searchTxt.sendKeys("לחם");
-			pof.Button1.click();
-			Thread.sleep(2000);
-			pof.result.get(3).click();
-			Thread.sleep(2000);
-			pof.quantity.sendKeys("2");
-			Thread.sleep(2000);
-			pof.add.click();
-			Thread.sleep(2000);
-			pof.result.get(3).click();
-			pof.result.get(4).click();
-			Thread.sleep(2000);
-			pof.add.click();
-			Thread.sleep(2000);
 			String first=pof.Carbo1.getText();
 			int first1=Integer.parseInt(first);
 			String secand=pof.Carbo2.getText();
@@ -288,24 +216,20 @@ public class t7_1 {
 			if (sumCarbo1==sum) {
 				System.out.println("הפחמימות שוות");
 				System.out.println("test11 pass");
-			}	
-
-		} catch (Exception e) {
-			throw new RuntimeException();
-		}
+			}	else {
+				throw new RuntimeException();
+			}
 	}
 
-
 	@Test
-	public void test012() {
-		try {
+	public void test012() throws InterruptedException {
+		
 			driver.get("https://www.nestle.com/jobs/search-jobs?keyword=&country=IL&location=&career_area=All&company=All#");
-			Thread.sleep(1500);
-			//JavascriptExecutor js = (JavascriptExecutor) driver;
-		//	js.executeScript("window.scrollBy(0,250)", "");
-			Thread.sleep(3000);
+			Thread.sleep(2000);	 
 			int count =0;
 		for (int i = 0; i < pof.pages.size(); i++) {
+		//	JavascriptExecutor js = (JavascriptExecutor) driver;
+			//js.executeScript("window.scrollBy(0,250)", "");
 			for (int j = 1; j < pof.jobs.size(); j++) {
 				count++;
 			}
@@ -313,8 +237,7 @@ public class t7_1 {
 		}
 			System.out.println(count);
 			System.out.println("test12 pass");
-		} catch (Exception e) {
-		}
+		
 	}
 	
 
