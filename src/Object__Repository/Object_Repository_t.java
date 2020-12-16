4a35d085429e9c5112e862ff00adc5c7bcc9741e
roi32 <75878108+roi32@users.noreply.github.com>
2020-12-16 21:14:53 +0200

    new

--- src/Object__Repository/Object_Repository_t.java
+++ src/Object__Repository/Object_Repository_t.java
@@ -1,7 +1,6 @@
 package Object__Repository;
 
 import java.io.IOException;
-
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.openqa.selenium.By;
@@ -12,21 +11,30 @@ public class Object_Repository_t {
 	public static WebDriver driver;
 
 
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, InterruptedException {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException  {
 		Object_Repository_cunfig or=new Object_Repository_cunfig(driver);
 		int bowserNum=1;
+		int categoryNum=1;
+		
 		while (bowserNum<=3) {
 			driver=or.browser(driver, or.getData("brwser"+bowserNum));
 			driver.manage().window().maximize();
-			while (or.getData("category"+bowserNum) != null) {
+			driver.switchTo().window(driver.getWindowHandle());
+			while (categoryNum<=3) {
 				driver.get("https://www.d.co.il/");
-				driver.findElement(By.id("query")).sendKeys(or.getData("category"+bowserNum));
+				driver.findElement(By.id("query")).sendKeys(or.getData("category"+categoryNum));
 				driver.findElement(By.className("search_submit")).click();
-				driver.close();
+				System.out.println(driver.getTitle());
+				categoryNum++;
 			}
 			bowserNum++;
+			driver.close();
+			
+
+
 		}
-
+		
+//
 	}
-
+//
 }
