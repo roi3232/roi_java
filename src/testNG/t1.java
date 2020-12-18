be7ae37687ee957b7c6402010eb1070e08ea6b82
roi32 <75878108+roi32@users.noreply.github.com>
2020-12-18 13:52:02 +0200

    1

--- src/testNG/t1.java
+++ src/testNG/t1.java
@@ -10,17 +10,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 
 public class t1 {
- static WebDriver driver;
+  static WebDriver driver;
 
-
-  @BeforeClass
-  public void beforeClass() {
-	
-		  
-  }
-
-  @AfterClass
-  public void afterClass() {
+  @Test
+  public void f() {
   }
   @Parameters({ "browser" })
   @BeforeTest
@@ -30,18 +23,24 @@ public class t1 {
 			  System.setProperty("webdriver.chrome.driver", "C:\\temp\\chromedriver.exe");
 				System.setProperty("webdriver.chrome.silentOutput", "true");
 			    driver=new ChromeDriver();
+			    driver.get("https://www.nisha.co.il/%D7%98%D7%91%D7%9C%D7%90%D7%95%D7%AA-%D7%A9%D7%9B%D7%A8");
 			    }
 		  else if ( browser.equalsIgnoreCase ("FF")) {
 			  System.setProperty("webdriver.gecko.driver", "C:\\temp\\geckodriver.exe");
 				System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"null");
 				driver = new FirefoxDriver();
+			    driver.get("https://www.nisha.co.il/%D7%98%D7%91%D7%9C%D7%90%D7%95%D7%AA-%D7%A9%D7%9B%D7%A8");
 				}
 		  }catch (Exception e) {
 		  }
   }
   
-  
-  @Test
-  public void test1() {
+  @BeforeClass
+  public void beforeClass() {
   }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
 }
