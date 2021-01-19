1d2394fbcea5ae35219ef8b3cced400e36977685
roi32 <75878108+roi32@users.noreply.github.com>
2021-01-19 15:13:04 +0200

    update

--- src/tigboor_func_loop/pitaronTargilFunc.java
+++ src/tigboor_func_loop/pitaronTargilFunc.java
@@ -8,20 +8,20 @@ import org.openqa.selenium.WebElement;
 
 public class pitaronTargilFunc {
 
-
-	
 	static List<WebElement> page;
-	
+
 	public static List<WebElement> list(WebDriver driver) {
-		List<WebElement> list =  driver.findElements(By.xpath("//div[@class='article']//h1"));
+		List<WebElement> list = driver.findElements(By.xpath("//div[@class='article']//h1"));
 		return list;
 	}
-	
+
 	public static List<WebElement> page(WebDriver driver) {
-		List<WebElement>page = driver.findElements(By.xpath("//div[@class='pagination-centered']//a[@class='pageLink']"));	
+		List<WebElement> page = driver
+				.findElements(By.xpath("//div[@class='pagination-centered']//a[@class='pageLink']"));
 		return page;
 	}
+
 	public static void printPage(int i) {
-		System.out.println("--- page : " + (i+1) + " ---");
+		System.out.println("--- page : " + (i + 1) + " ---");
 	}
 }
